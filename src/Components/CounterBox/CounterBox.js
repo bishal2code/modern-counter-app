baa704309f2a9e096a594339a2db8cedab548102
bishal2code <bishaldhakalinfo@gmail.com>
2026-03-30 12:00:36 +0545

    Make Design Complete And Theme Changer Make Working

--- src/Components/CounterBox/CounterBox.js
+++ src/Components/CounterBox/CounterBox.js
@@ -1,71 +1,76 @@
+import { useEffect, useState } from "react";
 import "./CounterBox.css";
 const CounterBox = () => {
+    const [theme, setTheme] = useState("dark");
+
+    function toggleTheme() {
+        setTheme(prev => (prev === "light" ? "dark" : "light"));
+    }
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+    }, [theme])
     return (
         <>
             <div className="counter_container">
                 <div className="themeChanger">
-                    <i className="fa-solid fa-sun"></i>
+                    <i onClick={toggleTheme} className="fa-solid fa-sun"></i>
                 </div>
+
                 <div className="counters">
                     <div className="counter">
                         <div className="timeBox">
-                            <p>0</p>
+                            <input type="text" value={"00"} />
+                            {/* <p>0</p>
                             <p>1</p>
                             <p>2</p>
                             <p>3</p>
-                            <p>4</p>
-                        </div>
-                    </div>
-                    <div className="timeLabel">Day</div>
-                </div>
-                <div className="counters">
-                    <div className="counter">
-                        <div className="timeBox">
-                            <p>0</p>
-                            <p>1</p>
-                            <p>2</p>
-                            <p>3</p>
-                            <p>4</p>
+                            <p>4</p> */}
                         </div>
                         <div className="timeLabel">Day</div>
                     </div>
                     <div className="counter">
                         <div className="timeBox">
-                            <p>0</p>
+                            <input type="text" value={"00"} />
+                            {/* <p>0</p>
                             <p>1</p>
                             <p>2</p>
                             <p>3</p>
-                            <p>4</p>
+                            <p>4</p> */}
                         </div>
                         <div className="timeLabel">Hours</div>
                     </div>
                     <div className="counter">
                         <div className="timeBox">
-                            <p>0</p>
+                            <input type="text" value={"00"} />
+                            {/* <p>0</p>
                             <p>1</p>
                             <p>2</p>
                             <p>3</p>
-                            <p>4</p>
+                            <p>4</p> */}
                         </div>
                         <div className="timeLabel">Minutes</div>
                     </div>
                     <div className="counter">
                         <div className="timeBox">
-                            <p>0</p>
+                            <input type="text" value={"00"} />
+                            {/* <p>0</p>
                             <p>1</p>
                             <p>2</p>
                             <p>3</p>
-                            <p>4</p>
+                            <p>4</p> */}
                         </div>
                         <div className="timeLabel">Seconds</div>
                     </div>
-                    
+
                 </div>
-                
+
                 <div className="actionButtons">
-                    <button> Start <i className="fa-solid fa-play"></i></button>
-                    <button> Start <i className="fa-solid fa-pause"></i></button>
-                    <button> Start <i className="fa-solid fa-x"></i></button>
+                    {/* <button className="mainBTN"> Start <i className="fa-solid fa-play"></i></button> */}
+                    <div className="secondBTN">
+                        <button> Pause <i className="fa-solid fa-pause"></i></button>
+                        <button> Clear <i className="fa-solid fa-x"></i></button>
+                    </div>
+
                 </div>
             </div>
         </>
